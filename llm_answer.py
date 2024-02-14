6dafb04c92af2eb7f68a215b13872ff334483f63
souravbiswas19 <souravbiswas19april@gmail.com>
2024-02-14 14:45:47 +0530

    Gen AI Assignment

--- llm_answer.py
+++ llm_answer.py
@@ -1,12 +1,16 @@
+"""This class implements the ConversationalRetrievalChain to answer the questions using Google's PaLM 2 LLM"""
 from langchain.chains import ConversationalRetrievalChain
-
+#fucntion to fetch the answer from chromadb using PaLM 2
 def fetch_answer(question, llm, db, chat_history):
+    """Returns the answer of the given question from ChromaDB using PaLM 2 LLM"""
+    #To create a conversational question-answering chain
     qa_chain = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(),verbose=False,return_source_documents=True)
+    #Store the answer after fetching it from the vector database chromadb
     result =  qa_chain.invoke({"question": question, "chat_history": chat_history})
-    print("Answer: " + result["answer"])
+    #stor the answer in chat history
     chat_history.append((question, result["answer"]))
     # Return the content of the most relevant document
     if result:
-        return result["answer"]
-    else:
-        return "No relevant information found."
+        return result["answer"] #return the answer fetched from the vector database
+    return "No relevant information found." #return this statement when no relevant information is fetched from the database
+#End of the File 
--- main.py
+++ main.py
@@ -9,14 +9,21 @@ from pdf_handler import process_pdf
 from chroma_db import upload_to_chromadb
 from llm_answer import fetch_answer
 google_api_key=fetch_google_key()
-llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=google_api_key)
-llm.temperature = 0.1
-chat_history=[]
 
+#Initializaing the Google PaLM 2 LLM. text-bison-001 is the name of the model
+llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=google_api_key)
+#Temperature of the LLM model is set. 
+#A lower temperature value makes the AI's responses more focused and deterministic.
+llm.temperature = 0.1
+#To store the history of the searched questions and it's answers
+chat_history=[]
+#Initializing FastAPI
 app = FastAPI()
+#POST api endpoint to accept the path and name of the pdf file and uplaod it to chromadb Database.
 @app.post("/upload/", status_code=status.HTTP_202_ACCEPTED)
 async def upload_file(pdf_path: str = None, pdf_name: str = None):
     """Function to upload a file"""
+    #joining the file path and pdf name to fetch it from the specified location
     file_path=os.path.join(pdf_path, pdf_name)
     #Handling the Exceptions if the file exists in the specified directory or not
     try:
@@ -32,9 +39,11 @@ async def upload_file(pdf_path: str = None, pdf_name: str = None):
         chroma_instances[coll_name] = db
         # Optionally, you can return some information
         return {"message": f"File '{pdf_name}' uploaded successfully and processed with collection name '{coll_name}'."}
+    #Exception encountered when the file doesn't exist at that paricular location
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e)) from e
     
+#GET API endpoint to search the question from the chromaDB database using Langchain 
 @app.get("/query/", status_code=status.HTTP_302_FOUND)
 async def query_pdf(pdf_name: str, question: str):
     """Query the function version."""
@@ -47,10 +56,13 @@ async def query_pdf(pdf_name: str, question: str):
         db = chroma_instances[pdf_name]
         #PaLM 2 Integration
         ans=fetch_answer(question, llm, db, chat_history)
+        #returns the answer after successful search from the pdf
         return {"answer": ans}
+    #Exception encountered if the pdf does not exist
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=str(e)) from e
-    
+
+#checks whether the current script is being run as the main program
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
